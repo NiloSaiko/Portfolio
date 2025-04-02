6bfa604d060c0f49c3871ba3c0d463133397921b
NiloSaiko <gwendallebras2006@gmail.com>
2025-04-02 20:48:15 +0200

    ajout script

---
+++ assets/js/navbar.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const burger = document.getElementById("burger");
+    const navLinks = document.getElementById("nav-links");
+
+    burger.addEventListener("click", function () {
+        navLinks.classList.toggle("active");
+        burger.classList.toggle("toggle");
+    });
+});
